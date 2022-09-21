b163fea57d32cb2b0aaf0c954d3c3ac119cbb32b
sidqiaverroes <sidqi.averroes26@gmail.com>
2022-09-21 19:28:22 +0700

    add initial files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# paw_inventaris
+#This is a Simple Inventory Management Web Application
+
+
+#Team Member
+Ariq
+
+Diva
+
+Miftah
+
+Naufal
+
+Sidqi 20/460568/TK/51157
---
+++ routes/inventaris.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+//getting all
+
+//getting one
+
+//creating one
+
+//updating one
+
+//deleting one
+
+module.exports = router
