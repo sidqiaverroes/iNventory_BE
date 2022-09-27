83c52a357bc710d6cc45730371a216c03e2daed8
AriqAryasuta <ariqaryasuta3@gmail.com>
2022-09-27 15:13:13 +0700

    Menambahkan fitur delete pada item inventory dalam controller

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,1 +1,17 @@
 const Inventory = require("../models/inventories");
+
+//Delete single inventory
+const deleteInventoryController = async (req, res) => {
+    try {
+      const invDel = await Inventory.findById(req.params.id);
+      if (invDel == null) {
+        return res.status(404).json({ message: "Cannot find inventory" });
+      }
+      await invDel.remove();
+      return res
+        .status(200)
+        .json({ message: "Sucessfully Deleted inventory: " + invDel.title });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
