5f7e97f39dfb01bcf9514f60ea5febc282fb6c08
sidqiaverroes <sidqi.averroes26@gmail.com>
2022-09-27 14:52:57 +0700

    add some folder and files to make the skeleton of the project and edit some models

--- models/inventories.js
+++ models/inventories.js
@@ -1,20 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const inventorySchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
 
   user_id: {
-    type: Number,
-    // required: true
+    type: String,
+    // required: true,
   },
-  
-  item_id: {
-    type: Number,
-  }
-
 });
 
-module.exports = mongoose.model('Inventory', inventorySchema)
+module.exports = mongoose.model("Inventory", inventorySchema);
