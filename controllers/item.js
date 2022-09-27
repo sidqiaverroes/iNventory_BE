5e4da7ab3dcd29a8e1b0e764f91efe27b48ca460
AriqAryasuta <ariqaryasuta3@gmail.com>
2022-09-27 15:18:05 +0700

    Add delete request for item on controller

--- controllers/item.js
+++ controllers/item.js
@@ -1,1 +1,17 @@
 const Item = require("../models/items");
+
+//Deleting
+const deleteItemController = async (req, res) => {
+    try {
+      const itemDel = await Item.findById(req.params.id);
+      if (itemDel == null) {
+        return res.status(404).json({ message: "Cannot find item" });
+      }
+      await itemDel.remove();
+      return res
+        .status(200)
+        .json({ message: "Sucessfully Deleted item: " + itemDel.name });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
