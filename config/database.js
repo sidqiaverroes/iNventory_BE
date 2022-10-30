a8d0dd6bc7939abf5917ad44e993cf049b29d32b
sidqiaverroes <sidqi.averroes26@gmail.com>
2022-10-31 05:56:14 +0700

    fix: database connection

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   const db = mongoose.connection;
   db.on("error", (error) => console.error(error));
   db.once("open", () => console.log("Connected to database"));
